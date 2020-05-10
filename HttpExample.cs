ec9f773a5949c70d84b470c37539bb87327ac958
Jimmy <jimmyjamesbaldwin@gmail.com>
2020-05-10 19:38:03 +0100

    asd

--- HttpExample.cs
+++ HttpExample.cs
@@ -16,11 +16,11 @@ namespace LocalFunctionProj
 {
     public static class HttpExample
     {
-        [FunctionName("HttpExample")]
+        [FunctionName("LocalFunctionProj")]
         public static HttpResponseMessage Run(HttpRequestMessage req, TraceWriter log)
         {
             var response = new HttpResponseMessage(HttpStatusCode.OK);
-            var stream = new FileStream(@"d:\home\site\wwwroot\LocalFunctionPro\index.html", FileMode.Open);
+            var stream = new FileStream(@"d:\home\site\wwwroot\LocalFunctionProj\index.html", FileMode.Open);
             response.Content = new StreamContent(stream);
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
             return response;
